2b77b43d793e5bae668135d44efddd615846a61a
fmomoreira <felipemoreira.frontend@gmail.com>
2020-07-22 10:54:22 -0300

    gerar qrcode com php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace chillerlan\QRCodeExamples;
+
+use chillerlan\QRCode\{QRCode, QROptions};
+
+require_once __DIR__.'./vendor/autoload.php';
+
+$data = 'https://123pagou.com.br/terminaldepagamento/';
+
+//quick and simple:
+
+
+$options = new QROptions([
+	'version'    => 5,
+	'outputType' => QRCode::OUTPUT_MARKUP_SVG,
+	'eccLevel'   => QRCode::ECC_L,
+]);
+
+// invoke a fresh QRCode instance
+$qrcode = new QRCode($options);
+
+// and dump the output
+$qrcode->render($data);
+
+// ...with additional cache file
+$qrcode->render($data, './img/file.svg');
